2a0ae709f320a45b71263819e5dda4136cfe838d
Anupam Anand <theanupamojha999@gmail.com>
2026-08-14 17:51:26 +0530

    feat add trip controller

---
+++ src/controllers/travel.controller.js
@@ -0,0 +1,25 @@
+import { travelAgent } from "../services/ai.service";
+
+export const generateTrip = async (req, res) => {
+    try{
+        const travelData = req.body ;
+
+    if (!travelData.place || !travelData.time || !travelData.budget) {
+      return res.status(400).json({
+        message: "Place, Time and Budget are required",
+      });
+    }
+
+        const result = await travelAgent(travelData);
+
+       return res.status(200).json(result)
+    }
+    catch(error){
+        console.error("Trip generation failed", error);
+
+        return res.status(500).json({
+            message: "Failed to generate trip. Please try again later"
+        })
+        
+    }
+} 
