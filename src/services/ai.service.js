acdf04abe53add6af9de082eee79329daa579c3a
Anupam Anand <theanupamojha999@gmail.com>
2026-08-14 16:34:30 +0530

    feat: add LLM service

---
+++ src/services/ai.service.js
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from "@google/genai";
+import travelPrompt from "../prompts/travel.prompt";
+import { json } from "express";
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
+
+export const travelAgent = async(travelData) => {
+const interaction = await ai.interactions.create({
+  model: "gemini-3.1-flash-lite",
+  input: travelPrompt(travelData),
+  response_format:{
+    type: "text",
+    mime_type:"application/json"
+  }
+});
+
+return JSON.parse(interaction.output_text);
+}
