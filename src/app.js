cc1c9c3a84d8fa7265e3083d70ba27d32da5b470
Anupam Anand <theanupamojha999@gmail.com>
2026-09-07 01:35:02 +0530

    add vercel links

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(cors({
     origin:[
-        'http://localhost:5174'
+        'http://localhost:5173',
+        'https://wandersoul-ai.vercel.app',
+        'https://wandersoul-ai-git-main-anupam-anand-ojhas-projects-0dac4885.vercel.app',
+        'https://wandersoul-i9a0yaj64-anupam-anand-ojhas-projects-0dac4885.vercel.app'
+        
     ],
     credentials:true
 }));
