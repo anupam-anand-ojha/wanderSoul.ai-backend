fd00a1540f048f25accfb5cbbdf2f79b8ca75a2f
Anupam Anand <theanupamojha999@gmail.com>
2026-09-13 22:00:03 +0530

    add chatbot ai service

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,19 +1,30 @@
 import { GoogleGenAI } from "@google/genai";
+
 import travelPrompt from "../prompts/travel.prompt.js";
+import chatPrompt from "../prompts/chat.prompt.js";
 
 const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
-export const travelAgent = async(travelData) => {
-const interaction = await ai.interactions.create({
-  model: "gemini-3.1-flash-lite",
-  input: travelPrompt(travelData),
-  response_format:{
-    type: "text",
-    mime_type:"application/json"
-  }
-});
+export const travelAgent = async (travelData) => {
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.1-flash-lite",
+    input: travelPrompt(travelData),
+    response_format: {
+      type: "text",
+      mime_type: "application/json",
+    },
+  });
 
-return JSON.parse(interaction.output_text);
-}
+  return JSON.parse(interaction.output_text);
+};
+
+export const chatAgent = async (message, history) => {
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.1-flash-lite",
+    input: chatPrompt(message, history),
+  });
+
+  return interaction.output_text;
+};
